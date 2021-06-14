f11463608b78649a9f623ba21af375dd42bef085
Saheersk <saheerabcd3@gmail.com>
2021-06-14 09:56:35 +0530

    Header-Section-Completed

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCslTvw2ioSElnIWclD6hZuz6PxC0zM4u0",
+    authDomain: "clone-85f9d.firebaseapp.com",
+    projectId: "clone-85f9d",
+    storageBucket: "clone-85f9d.appspot.com",
+    messagingSenderId: "579581268716",
+    appId: "1:579581268716:web:3f4e911843403287e80bc9",
+    measurementId: "G-D06DYLR0GY"
+  };
