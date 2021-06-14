100989acac68ebba3323c64c6887266d5c9ff8a2
Saheersk <saheerabcd3@gmail.com>
2021-06-14 16:50:47 +0530

    Checkout-Page_completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import Header from './components/screens/Header';
-import Home from './components/screens/Home';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/screens/Header";
+import Home from "./components/screens/Home";
+import Checkout from "./components/screens/Checkout";
+import Login from "./components/screens/Login";
 
 function App() {
   return (
+      <Router>
     <div className="App">
-     <Header />
-     <Home />
+      <Header />
+        <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/checkout" exact component={Checkout} />
+        <Route path="/login" exact component={Login} />
+        </Switch>
     </div>
+      </Router>
   );
 }
 
--- src/components/screens/Product.js
+++ src/components/screens/Product.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import './Product.css'
-function Product({ title, image, price, rating }) {
-    return (
-        <div className="product">
-          <div className="product__info">
-            <p>{title}</p>
-            <p className="product__price">
-              <small>$</small>
-              <strong>{price}</strong>
-            </p>
-            <div className="product__rating">
-              {Array(rating).fill().map((_, i) => (
-                  <p>🌟</p>
-                ))}
-            </div>
-          </div>
-    
-          <img src={image} alt="" />
-    
-          <button>Add to Basket</button>
+import React from "react";
+import { useStateValue } from "../../StateProvider";
+import "./Product.css";
+
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
-      );
+      </div>
+
+      <img src={image} alt="" />
+
+      <button onClick={addToBasket}>Add to Basket</button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
